bb2f5010d5b67681253bffe2f4600003e58ab9d9
Nicolas Fiasche <fiaschenicolas@gmail.com>
2020-09-16 14:53:10 -0300

    Deleted files

--- app1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hola');
-let nombre = 'ignacio';
-let apellido = '1234';
-let cd = 'asd';
--- app1.ts
+++
@@ -1,5 +0,0 @@
-export {};
-console.log('hola');
-let nombre:string = 'ignacio';
-let apellido:string = '1234';
-let cd:string ='asd';
