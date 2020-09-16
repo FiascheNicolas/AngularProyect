abb9af1c3c0aef671fec571f91c49b061393ee4f
Nicolas Fiasche <fiaschenicolas@gmail.com>
2020-09-16 14:43:17 -0300

    Adding test files

---
+++ app1.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('hola');
+let nombre = 'ignacio';
+let apellido = '1234';
+let cd = 'asd';
---
+++ app1.ts
@@ -0,0 +1,5 @@
+export {};
+console.log('hola');
+let nombre:string = 'ignacio';
+let apellido:string = '1234';
+let cd:string ='asd';
